0e2cffd314123afadd6154d9a81f0a43627d7c8e
Kgaugelo <mthethwakl15@gmail.com>
2024-01-31 11:18:20 +0200

    Project Start

---
+++ src/main/java/com/example/GeeksChatSpringBootBackend/GeeksChatSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.GeeksChatSpringBootBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeksChatSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeksChatSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GeeksChatSpringBootBackend/GeeksChatSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GeeksChatSpringBootBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeksChatSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
