ac35c9f10cd7c0bab15e827b9f0fb3879012efc7
Kgaugelo <mthethwakl15@gmail.com>
2024-02-01 10:32:16 +0200

    Message Model Updated

--- src/main/java/com/example/GeeksChatSpringBootBackend/model/Message.java
+++ src/main/java/com/example/GeeksChatSpringBootBackend/model/Message.java
@@ -1,4 +1,28 @@
 package com.example.GeeksChatSpringBootBackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long messageId;
+    @ManyToOne
+    @JoinColumn(name="SenderID")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name="ReceiverID")
+    private User receiver;
+    private String messageContent;
+    private LocalDateTime messageTime;
+    private boolean isRead;
+    private boolean isReceived;
 }
