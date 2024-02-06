768fee165ca19840e5aaa7f979542d2053f4c3e3
Kgaugelo <mthethwakl15@gmail.com>
2024-02-06 14:05:23 +0200

    Controllers Updated
    Controllers Updated

--- src/main/java/com/example/GeeksChatSpringBootBackend/contollers/MessageController.java
+++ src/main/java/com/example/GeeksChatSpringBootBackend/contollers/MessageController.java
@@ -1,4 +1,9 @@
 package com.example.GeeksChatSpringBootBackend.contollers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Message")
 public class MessageController {
 }
